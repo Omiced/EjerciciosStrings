c7935ddb72854893bc57305016ab50a205673cb4
Josue Cano <73917299+Omiced@users.noreply.github.com>
2022-12-12 19:56:11 +0000

    Terminado ejercicio 2

--- js/main.js
+++ js/main.js
@@ -14,3 +14,13 @@ const repeated = function (str) {
 };
 
 console.log(repeated("Abraham"));
+
+const reverseString = function (str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return reversed;
+};
+console.log(reverseString("Cano"));
