faba48c172792608cfb9b89033bb935f588a356e
Josue Cano <73917299+Omiced@users.noreply.github.com>
2022-12-12 23:13:51 +0000

    Agregada regex validación email

--- js/main.js
+++ js/main.js
@@ -9,9 +9,15 @@ const alertError = document.getElementById("alert");
 btnEnviarEl.addEventListener("click", (e) => {
   e.preventDefault();
   alertError.style.display = "none";
+  const emailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!exampleFormControlInput1.value.match(emailRegex)) {
+    alertError.style.display = "block";
+    alertError.innerHTML += "Correo invalido PErro";
+  }
   if (exampleFormControlTextarea1.value.replaceAll("  ", "").length < 20) {
     alertError.style.display = "block";
-    alertError.textContent = "PErro";
+    alertError.innerHTML += "PErro";
     exampleFormControlTextarea1.focus();
   }
 });
