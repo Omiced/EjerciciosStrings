dcb735f208f55421ffb08f9d46934b4429cfccfd
Josue Cano <73917299+Omiced@users.noreply.github.com>
2022-12-13 17:10:44 +0000

    Realizada validación de rfc con una regex desde 0

--- js/main.js
+++ js/main.js
@@ -6,18 +6,30 @@ const exampleFormControlTextarea1 = document.getElementById(
   "exampleFormControlTextarea1"
 );
 const alertError = document.getElementById("alert");
+const rfcInputEl = document.getElementById("rfc-input");
+// btnEnviarEl.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   alertError.style.display = "none";
+//   const emailRegex =
+//     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   if (!exampleFormControlInput1.value.match(emailRegex)) {
+//     alertError.style.display = "block";
+//     alertError.innerHTML += "Correo invalido PErro";
+//   }
+//   if (exampleFormControlTextarea1.value.replaceAll("  ", "").length < 20) {
+//     alertError.style.display = "block";
+//     alertError.innerHTML += "PErro";
+//     exampleFormControlTextarea1.focus();
+//   }
+// });
+
 btnEnviarEl.addEventListener("click", (e) => {
   e.preventDefault();
   alertError.style.display = "none";
-  const emailRegex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!exampleFormControlInput1.value.match(emailRegex)) {
+  let RFCRegex = /^[A-Z]{4}[0-9]{6}[A-Z0-9]{3}$/;
+  if (rfcInputEl.value.match(RFCRegex) == null) {
     alertError.style.display = "block";
-    alertError.innerHTML += "Correo invalido PErro";
-  }
-  if (exampleFormControlTextarea1.value.replaceAll("  ", "").length < 20) {
-    alertError.style.display = "block";
-    alertError.innerHTML += "PErro";
+    alertError.innerHTML += "El RFC no es válido";
     exampleFormControlTextarea1.focus();
   }
 });
